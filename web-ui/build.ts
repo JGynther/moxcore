0428f595f74ba8d58f29ac53c71c09c29eaf661a
Joona Gynther <joona@gynther.xyz>
2024-09-28 00:03:14 +0300

    add build version info

---
+++ web-ui/build.ts
@@ -0,0 +1,12 @@
+import { $ } from "bun";
+
+const rev = (await $`git rev-parse HEAD`.text()).slice(0, 6);
+
+const version = {
+    rev,
+};
+
+const bytes = await Bun.write("./src/version.json", JSON.stringify(version));
+
+console.log(`Current rev: ${rev}`);
+console.log(`Wrote ${bytes} bytes`);
