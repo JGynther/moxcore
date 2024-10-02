45624e1431b5134c979a21d452c8c02a45c7019d
Joona Gynther <joona@gynther.xyz>
2024-10-02 04:01:29 +0300

    add react-compiler

--- www/rsbuild.config.ts
+++ www/rsbuild.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginSass } from "@rsbuild/plugin-sass";
+import { pluginBabel } from "@rsbuild/plugin-babel";
 
 export default defineConfig({
     html: {
         title: "Mirror Mox",
     },
-    plugins: [pluginReact(), pluginSass()],
+    plugins: [
+        pluginReact(),
+        pluginSass(),
+        pluginBabel({
+            include: /\.(?:jsx|tsx)$/,
+            babelLoaderOptions(opts) {
+                opts.plugins?.unshift("babel-plugin-react-compiler");
+            },
+        }),
+    ],
 });
