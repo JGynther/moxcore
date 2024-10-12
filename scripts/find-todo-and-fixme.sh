5bce978374a961a6ca9e5488035ea27e7bb3039b
Joona Gynther <joona@gynther.xyz>
2024-10-13 02:53:59 +0300

    update fixme finder script to exclude itself

--- scripts/find-todo-and-fixme.sh
+++ scripts/find-todo-and-fixme.sh
@@ -1,1 +1,2 @@
-grep -R -E "TODO|FIXME" --exclude-dir=node_modules --exclude-dir=env * 
+# Excluding "scripts" dir to not find itself...
+grep -R -E "TODO|FIXME" --exclude-dir=node_modules --exclude-dir=.venv * --exclude-dir=scripts
