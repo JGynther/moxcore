6a18258b35cba18d85ed5afb75b058cdb7ddaeb2
Joona Gynther <joona@gynther.xyz>
2025-02-27 05:33:33 +0200

    add rest of applicable permanents to special masks

--- mox-compiler/mox/normalize.py
+++ mox-compiler/mox/normalize.py
@@ -5,9 +5,14 @@ def normalize(input: str):
     input = input.lower()
     input = re.compile(r".*\s?—\s").sub("", input)
     input = re.compile(r"\([^)]+\)").sub("", input)
+
+    # Special cases
     input = input.replace("this spell", "[[card]]")
     input = input.replace("this creature", "[[card]]")
     input = input.replace("this aura", "[[card]]")
+    input = input.replace("this enchantment", "[[card]]")
+    input = input.replace("this artifact", "[[card]]")
+    input = input.replace("this land", "[[card]]")
 
     return input
 
