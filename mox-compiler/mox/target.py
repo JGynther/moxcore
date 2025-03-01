2e7cd9a96c4a3678951da641fec73b064781c3d5
Joona Gynther <joona@gynther.xyz>
2025-03-01 11:51:39 +0200

    significantly improve target extraction, low estimate is 78.5% of targets correctly parsed

--- mox-compiler/mox/target.py
+++ mox-compiler/mox/target.py
@@ -1,21 +1,94 @@
 import re
 
+TARGET_END_SEQUENCES = r"|\b".join(
+    [
+        "$",
+        "\\.",
+        "able",
+        "add",
+        "and[^\\/]",
+        "are",
+        "as",
+        "becomes",
+        "blocks",
+        "by",
+        "can",
+        "can't",
+        "chooses?",
+        "cost",
+        "creates?",
+        "discards?",
+        "does",
+        "doesn't",
+        "draws?",
+        "during",
+        "equal",
+        "except",
+        "exchange",
+        "exiles?",
+        "explores?",
+        "face",
+        "fights?",
+        "for",
+        "from",
+        "gains?",
+        "gets?",
+        "hand",
+        "has",
+        "have",
+        "in",
+        "it",
+        "looks?",
+        "loses?",
+        "maximum",
+        "may",
+        "mills?",
+        "on the",
+        "phase",
+        "prevent",
+        "put",
+        "reveals",
+        "sacrifices?",
+        "scries",
+        "search",
+        "shuffles?",
+        "skips?",
+        "then",
+        "this",
+        "to",
+        "unless",
+        "until",
+        "up",
+        "where",
+        "would",
+        "you",
+    ]
+)
+
+end_sequence = rf"(?=\b{(TARGET_END_SEQUENCES)})"
+
+# Instances of "target" optionally prefixed by "up to [...]" or "another" statements
+direct_target = r"((up to \w+ )|another )?\btarget(?! of\b)"
+
+# Instances of "all" not followed by "combat" or "damage"
+all_target = r"\ball(?!\ combat|\ damage)"
+
+# Instances of "any" followed by allowlist literals
+any_target = r"\bany (?=creatures|target|other (?!type))"
+
+each_opponent = "each opponent"
+
+# Combined start sequence
+start_sequence = rf"({direct_target}|{all_target}|{any_target}|{each_opponent}).*?"
+
+TARGET_PATTERN = re.compile(start_sequence + end_sequence)
+
 
 def try_to_figure_out_target(effect: str) -> tuple[str | None, str]:
-    # Special cases
-    for case in ["any target", "each opponent", "target player", "targets"]:
-        if case in effect:
-            return case, effect.replace(case, "[[target]]")
-
-    match re.search(r"(target|all)[^.,]+", effect):
+    match TARGET_PATTERN.search(effect):
         case None:
             return None, effect
 
         case match:
-            target = match.group(0).replace("target", "").strip()
-
-            # Arbitrary limit for sanity check
-            if not target or len(target.split()) > 6:
-                return None, effect
-
-            return target, effect.replace(match.group(0), "[[target]]")
+            target = match.group(0).strip().rstrip(",")
+            return target, effect.replace(target, "[[target]]")
