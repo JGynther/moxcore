20a380a2305ef3c4ca83a39097a38c44b60d3439
Joona Gynther <joona@gynther.xyz>
2025-03-01 19:21:20 +0200

    new cache for processed virtual cards (load time ~1.5s -> 0.04s)

---
+++ similarity/cards/cache.py
@@ -0,0 +1,79 @@
+import pickle
+import typing as T
+from pathlib import Path
+from time import time
+
+import requests
+from cards.card import VirtualCard
+from cards.process_dump import process_scryfall_dump
+from msgspec import json
+from search.utils import timer
+
+CACHE = Path(".cache")
+DATA = CACHE / "data.pkl"
+META = CACHE / ".meta.json"
+TTL_IN_S = 432_000  # 5 days
+
+
+@timer
+def get_scryfall_data_from_cache() -> list[VirtualCard]:
+    refresh_cache_if_needed()
+    return pickle.loads(DATA.read_bytes())
+
+
+class Metadata(T.TypedDict):
+    timestamp: float
+    raw_file: str
+
+
+def write_metadata(raw_file_name: str):
+    data = {"timestamp": time(), "raw_file": raw_file_name}
+    META.write_bytes(json.encode(data))
+
+
+def get_metadata() -> Metadata | None:
+    if not META.exists():
+        return None
+
+    return json.decode(META.read_bytes())
+
+
+def refresh_cache_if_needed():
+    CACHE.mkdir(exist_ok=True)
+    metadata = get_metadata()
+    current_time = time()
+
+    if DATA.exists() and metadata:
+        if current_time - metadata["timestamp"] < TTL_IN_S:
+            return print("Using cached data.")
+
+    print("Downloading new Scryfall dump.")
+
+    file_name = download_scryfall_dump()
+    processed = process_scryfall_dump(file_name)
+    DATA.write_bytes(pickle.dumps(processed))
+    write_metadata(file_name)
+
+    return True
+
+
+# https://scryfall.com/docs/api/bulk-data
+def download_scryfall_dump() -> str:
+    BULK_URI = "https://api.scryfall.com/bulk-data"
+    response = requests.get(BULK_URI).json()
+
+    download_uri = ""
+
+    for each in response["data"]:
+        if each["type"] == "oracle_cards":
+            download_uri = each["download_uri"]
+
+    file_name = download_uri.split("/")[-1]
+    file_name = f".cache/{file_name}"
+
+    dump = requests.get(download_uri).text
+
+    with open(file_name, "w") as file:
+        file.write(dump)
+
+    return file_name
--- similarity/cards/process_dump.py
+++ similarity/cards/process_dump.py
@@ -1,16 +1,9 @@
-from datetime import datetime, timedelta
-from pathlib import Path
-
-import requests
-from msgspec import json
-
 from cards.card import Card, VirtualCard
 from cards.utils import should_skip_card
+from msgspec import json
 
 
-def process_scryfall_dump():
-    name = find_or_get_scryfall_dump()
-
+def process_scryfall_dump(name: str):
     with open(name) as file:
         content = file.read()
         cards_json = json.decode(content)
@@ -41,56 +34,3 @@ def write_cards_json(cards: list[VirtualCard], name="cards.experimental.json"):
 
     with open(name, "wb") as file:
         file.write(json.encode(data))
-
-
-def find_or_get_scryfall_dump() -> str:
-    directory = Path(".cache")
-    directory.mkdir(exist_ok=True)
-
-    match should_update_cache(directory):
-        case True, None:
-            return download_scryfall_dump()
-        case False, file_name:
-            return file_name
-
-
-CacheResult = tuple[True, None] | tuple[False, str]
-
-
-def should_update_cache(dir: Path) -> CacheResult:
-    files = list(dir.glob("oracle-cards-*.json"))
-
-    if not files:
-        return True, None
-
-    latest = max(files, key=lambda x: x.stat().st_mtime)
-    mod_time = datetime.fromtimestamp(latest.stat().st_mtime)
-    age = datetime.now() - mod_time
-
-    # Only update dump every ~5 days
-    if age < timedelta(days=5):
-        return False, latest.as_posix()
-
-    return True, None
-
-
-# https://scryfall.com/docs/api/bulk-data
-def download_scryfall_dump() -> str:
-    BULK_URI = "https://api.scryfall.com/bulk-data"
-    response = requests.get(BULK_URI).json()
-
-    download_uri = ""
-
-    for each in response["data"]:
-        if each["type"] == "oracle_cards":
-            download_uri = each["download_uri"]
-
-    file_name = download_uri.split("/")[-1]
-    file_name = f".cache/{file_name}"
-
-    dump = requests.get(download_uri).text
-
-    with open(file_name, "w") as file:
-        file.write(dump)
-
-    return file_name
