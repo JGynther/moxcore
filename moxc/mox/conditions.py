8ad5113446f3b97ce79c4164abb69bd9dc041368
Joona Gynther <joona@gynther.xyz>
2025-03-08 00:50:15 +0200

    fix condition splitting

--- moxc/mox/conditions.py
+++ moxc/mox/conditions.py
@@ -1,8 +1,10 @@
 import re
 
+condition_pattern = re.compile(r"\bunless\b|\bif\b")
+
 
 def split_condition_from_effect(effect: str) -> tuple[str | None, str]:
-    match re.search(r"\bunless\b|\bif\b", effect):
+    match condition_pattern.search(effect):
         case None:
             return None, effect
 
@@ -10,7 +12,7 @@ def split_condition_from_effect(effect: str) -> tuple[str | None, str]:
             if "," in effect:
                 condition, effect = effect.split(",", 1)
             else:
-                effect, condition = effect.split(match.group(0), 1)
+                effect, condition = re.split(rf"\b{match.group(0)}\b", effect, 1)
                 condition = match.group(0) + condition
 
             return condition, effect
