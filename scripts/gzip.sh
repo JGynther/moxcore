9ba00452881b10710ff8adb3dac4773723598b49
Joona Gynther <joona@gynther.xyz>
2025-03-18 18:38:16 +0200

    fix gzip script

--- scripts/gzip.sh
+++ scripts/gzip.sh
@@ -1,2 +1,2 @@
-cp similarity/cards.experimental.json www/public/cards.experimental.json
-gzip www/public/cards.experimental.json -f
+cp similarity/cards.experimental.json moxcore.com/public/cards.experimental.json
+gzip moxcore.com/public/cards.experimental.json -f
