a47f6ee4232ff0187aea4dfe1d00eaeb687bb30a
Joona Gynther <joona@gynther.xyz>
2025-04-21 10:19:44 +0300

    use mox normalize for bm25

--- similarity/pipeline.py
+++ similarity/pipeline.py
@@ -1,6 +1,6 @@
 from cards.cache import get_scryfall_data_from_cache
 from cards.process_dump import write_cards_json
-from cards.utils import normalize_oracle_text
+from mox import normalize
 from search.bm25 import batch_bm25_search
 from search.mox import compile_all_seq, score_all
 
@@ -9,7 +9,7 @@ TOP_K = 10
 
 cards = get_scryfall_data_from_cache()
 
-corpus = [normalize_oracle_text(card) or NULL for card in cards]
+corpus = [normalize(card.oracle_text, card.name) or NULL for card in cards]
 results = batch_bm25_search(corpus)
 
 # Mox
