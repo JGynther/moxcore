53932d0a577c4afb787b7f048ba84b86676b5666
Joona Gynther <joona@gynther.xyz>
2025-04-21 11:10:30 +0300

    split pretty_print into format+print defs

--- moxc/mox/__init__.py
+++ moxc/mox/__init__.py
@@ -1,10 +1,11 @@
 from mox.normalize import normalize
 from mox.parser import Card, parse
-from mox.utils import pretty_print
+from mox.utils import pretty_format, pretty_print
 
 __all__ = [
     "parse",
     "Card",
     "pretty_print",
+    "pretty_format",
     "normalize",
 ]
--- moxc/mox/utils.py
+++ moxc/mox/utils.py
@@ -2,27 +2,40 @@ from mox.data_types import Card
 
 
 def pretty_print(card: Card, print_name=True):
-    if print_name:
-        print(card.name)
-        print("--")
+    print(pretty_format(card, print_name))
 
-    print("TYPE", card.type)
-    print("KEYWORDS", card.keywords)
-    print()
+
+def pretty_format(card: Card, print_name=True) -> str:
+    lines = []
+
+    if print_name:
+        lines.append(card.name)
+        lines.append("--")
+
+    lines.append(f"TYPE {card.type}")
+    lines.append(f"KEYWORDS {card.keywords}")
+    lines.append("")
 
     for ability in card.abilities:
-        print(ability.category, ability.cost or "", ability.trigger or "")
-        print("│")
+        lines.append(f"{ability.category} {ability.cost or ''} {ability.trigger or ''}".strip())
+        lines.append("│")
 
         for index, effect in enumerate(ability.effects):
             is_more = index + 1 < len(ability.effects)
 
-            print(f"{'├' if is_more else '└'}─ {effect.type}")
-            print(f"{'│' if is_more else ' '}  ├──[text]        {effect.text}")
-            print(f"{'│' if is_more else ' '}  ├──[target]      {effect.target}")
-            print(f"{'│' if is_more else ' '}  ├──[condition]   {effect.condition}")
-            print(f"{'│' if is_more else ' '}  ├──[source]      {effect.source}")
-            print(f"{'│' if is_more else ' '}  └──[destination] {effect.destination}")
-            print("│" if is_more else "")
+            branch = "├" if is_more else "└"
+            pipe = "│" if is_more else " "
 
-    print()
+            lines.append(f"{branch}─ {effect.type}")
+            lines.append(f"{pipe}  ├──[text]        {effect.text}")
+            lines.append(f"{pipe}  ├──[target]      {effect.target}")
+            lines.append(f"{pipe}  ├──[condition]   {effect.condition}")
+            lines.append(f"{pipe}  ├──[source]      {effect.source}")
+            lines.append(f"{pipe}  └──[destination] {effect.destination}")
+
+            if is_more:
+                lines.append("│")
+
+        lines.append("")  # trailing newline
+
+    return "\n".join(lines)
